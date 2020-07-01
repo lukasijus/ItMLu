0cad62cf69eb5cc9467c9a572b663f933fab3395
Lukas Rimkus <lukasrimkuslg@gmail.com>
2020-07-01 23:08:57 +0200

    some

--- Lesson_3/fashionmnsitNN.py
+++ Lesson_3/fashionmnsitNN.py
@@ -10,6 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import nrml
 import plotImages
+import supp
 print(tf.__version__)
 
 # Build dataset (use only once)
@@ -34,11 +35,11 @@ test_numbers = metadata.splits['test'].num_examples
 print('Training number examples {}'.format(train_numbers))
 print('Test number examples {}'.format(test_numbers))
 
-train_dataset = train_dataset.map(nrml.normalize)
-test_dataset = test_dataset.map(nrml.normalize)
+# train_dataset = train_dataset.map(nrml.normalize)
+# test_dataset = test_dataset.map(nrml.normalize)
 
 # Iteration parameters
-BATCH_SIZE = 32
+BATCH_SIZE = 16
 train_dataset = train_dataset.repeat().shuffle(train_numbers).batch(BATCH_SIZE)
 test_dataset = test_dataset.batch(BATCH_SIZE)
 
@@ -58,7 +59,7 @@ model.compile(
 # V Train the model V
 model.fit(
     train_dataset,
-    epochs=3,
+    epochs=1,
     steps_per_epoch=math.ceil(train_numbers/BATCH_SIZE)
 )
 
@@ -67,30 +68,20 @@ test_loss, test_accuracy = model.evaluate(
     test_dataset,
     steps=math.ceil(test_numbers/BATCH_SIZE)
 )
-np.set_printoptions(precision=2)
+
 print('Accuracy of the model:', test_accuracy)
 
 # Make predictions
 batchCount = 1
 i = 0
+name = '2 layer Neural Network' + ', input neuron: ' + str(INPUT_NEURONS)
 for test_images, test_labels in test_dataset.take(batchCount):
     test_images = test_images.numpy()
     test_labels = test_labels.numpy()
     predictions = model.predict(test_images)
     i += 1
-    plotImages.plotbatch(test_labels,class_names,predictions,test_images)
+    plotImages.plotbatch(name,test_loss,test_accuracy,BATCH_SIZE,test_labels,class_names,predictions,test_images)
 
-# Review
-print('Shape of the prediction array: ', predictions.shape)
-print('The predicted class names for the n batch: ', predictions[0])
-np.set_printoptions(precision=2)
-print('Sort by Highest rated: ', np.sort(predictions[0])[::-1])
-print('Labels: ', test_labels[0])
-print('Labels shape: ', test_labels.shape)
-print('Shape of predicted batch labels: ', predictions.shape)
-print('Shape of predicted batch image: ', test_images.shape)
-print('Shape of predicted labels: ', predictions[0].shape)
-print('Shape of predicted image: ', test_images[0].shape)
 
 
 
--- Lesson_3/plotImages.py
+++ Lesson_3/plotImages.py
@@ -1,34 +1,45 @@
 # This function plots images
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import math
+
+from colour import Color
+
+red = Color("red")
+
 def plotimages(class_names, dataset, imageCount):
     i = 0
     for image, label in dataset.take(imageCount):
         image = image.numpy().reshape((28, 28))
         x_axis_length = int(round(math.sqrt(imageCount)))
         y_axis_length = int(round(math.sqrt(imageCount)))
-        plt.subplot(x_axis_length, y_axis_length, i + 1)
-        plt.xticks([])
-        plt.yticks([])
-        plt.imshow(image, cmap=plt.cm.binary)
+        fig, ax = plt.subplot(x_axis_length, y_axis_length, i + 1)
+        ax.imshow(image, cmap=plt.cm.binary)
+        rect = patches.Rectangle((1, 1), 26, 26, linewidth=1, edgecolor='r', facecolor='none')
+        ax.add_patch(rect)
         plt.grid(False)
         plt.xlabel(class_names[label])
         i = i + 1
     plt.show()
 
-def plotbatch(test_labels,class_names,predictions,test_images):
+def plotbatch(name, loss, accuracy, batch_size, test_labels,class_names,predictions,test_images):
     index = 0
+    loss = float("{:.2f}".format(loss))
+    accuracy = float("{:.2f}".format(accuracy))
+    fig, axes = plt.subplots(nrows=int(round(math.sqrt(batch_size))), ncols=int(round(math.ceil(math.sqrt(batch_size)))))
+    fig.suptitle(name + '\n' + 'Loss: ' + str(loss) + ' ' + 'Accuracy: ' + str(accuracy))
+    axes = axes.flatten()
+    plt.subplots_adjust(
+        left=0.03,
+        bottom=0.13,
+        right=0.98,
+        top=0.93,
+        wspace=0.95,
+        hspace=0.98)
+    plt.grid(False)
+    plt.axis('off')
     for image in test_images:
-        plt.subplot(4,8, index + 1)
-        plt.subplots_adjust(
-                    left=0.03,
-                    bottom=0.13,
-                    right=0.98,
-                    top=0.98,
-                    wspace=0.95,
-                    hspace=0.98)
-        plt.grid(False)
-        plt.imshow(test_images[index].reshape((28,28)), cmap=plt.cm.binary)
+        rect = patches.Rectangle((0, 0), 27, 27, linewidth=5, edgecolor='g', facecolor='none')
         class_name_index = 0
         highest_prediction = 0
         highest_prediction_index = 0
@@ -37,8 +48,15 @@ def plotbatch(test_labels,class_names,predictions,test_images):
                 highest_prediction = value
                 highest_prediction_index = class_name_index
             class_name_index += 1
-        plt.xlabel('P: ' + str(highest_prediction) +
+        axes[index].set_xlabel('P: ' + str(float("{:.2f}".format(highest_prediction))) +
                    '\n' + 'PC: ' + class_names[highest_prediction_index] +
                    '\n' + 'GTC: ' + class_names[test_labels[index]])
+        if highest_prediction_index != test_labels[index]:
+            rect = patches.Rectangle((0, 0), 27, 27, linewidth=5, edgecolor='r', facecolor='none')
+        axes[index].add_patch(rect)
+        axes[index].xaxis.set_label_coords(0.05, -0.025)
+        axes[index].imshow(test_images[index].reshape((28,28)), cmap=plt.cm.binary)
+        axes[index].set_yticklabels([])
+        axes[index].set_xticklabels([])
         index += 1
     plt.show()
